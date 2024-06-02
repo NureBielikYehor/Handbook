3c6f3481fbf67a342a924cc3365177c86b2c976e
Yehor Bielik <yehor.bielik@nure.ua>
2024-06-02 19:18:39 +0300

    rename form1

--- Handbook/Forms/MainForm.Designer.cs
+++ Handbook/Forms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Handbook
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             groupBox1 = new GroupBox();
--- Handbook/Forms/MainForm.cs
+++ Handbook/Forms/MainForm.cs
@@ -5,10 +5,10 @@ using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Handbook
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
         private PostalHandbook handbook;
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
 
